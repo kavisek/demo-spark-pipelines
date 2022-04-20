8cb803c6948e222e7858bee22309f069e45188f1
kavisek <kavi.skhon@gmail.com>
2022-04-19 22:22:29 -0700

    feat: alias with joins now works

--- notebooks/jobs/solana.py
+++ notebooks/jobs/solana.py
@@ -61,26 +61,25 @@ class SparkJob:
         # df.printSchema()
         return df
 
-    def solona_head(self) -> None:
+    def solana_head(self) -> None:
         """Head of Alias."""
         df = self.preprocessing_timeseries()
-        df = df.alias("s")
         df.printSchema()
         df.show(10)
 
-    def solona_aliased(self) -> None:
+    def solana_aliased(self) -> None:
         """Alias the dataset."""
         df = self.preprocessing_timeseries()
         df = df.alias("s")
         df.printSchema()
         df.show(10)
 
-    def solona_counts(self) -> None:
+    def solana_counts(self) -> None:
         """Provide the shape of the dataset."""
         df = self.preprocessing_timeseries()
         print(f"count: {df.count()}, {len(df.columns)}")
 
-    def solona_describe(self) -> None:
+    def solana_describe(self) -> None:
         """Describe the dataset."""
         df = self.preprocessing_timeseries()
         df.describe().show()
@@ -88,7 +87,7 @@ class SparkJob:
         
     # GROUP BY
         
-    def solona_groupby_avg(self) -> None:
+    def solana_groupby_avg(self) -> None:
         """Groupby and return max value of all columns."""
         df = self.preprocessing_timeseries()
 
@@ -98,7 +97,7 @@ class SparkJob:
         df = df.groupBy("month").max()
         df.show(4)
 
-    def solona_groupby_mean_single(self) -> None:
+    def solana_groupby_mean_single(self) -> None:
         """Groupby mean single column."""
         df = self.preprocessing_timeseries()
 
@@ -109,7 +108,7 @@ class SparkJob:
         df = df.groupBy("month").mean("open")
         df.show(4)
 
-    def solona_groupby_min_multiple(self) -> None:
+    def solana_groupby_min_multiple(self) -> None:
         """Groupby get mins from dataset."""
         df = self.preprocessing_timeseries()
 
@@ -120,7 +119,7 @@ class SparkJob:
         df = df.groupBy("month").min("open", "close")
         df.show(4)
 
-    def solona_groupby_custom(self) -> None:
+    def solana_groupby_custom(self) -> None:
         """Groupby with custom aggregation."""
         df = self.preprocessing_timeseries()
 
@@ -143,7 +142,7 @@ class SparkJob:
         
     # DISTINCT
 
-    def solona_months_distinct(self) -> None:
+    def solana_months_distinct(self) -> None:
         """Distinct Operation"""
         df = self.preprocessing_timeseries()
 
@@ -156,14 +155,14 @@ class SparkJob:
     
     # OTHERWISE
     
-    def solona_when(self) ->  DataFrame:
+    def solana_when(self) ->  DataFrame:
         """When Operation"""
         df = self.preprocessing_timeseries()
         df = df.select("open",when(df.open == 3, 1).otherwise(0).alias("case1"))
         return df
     
     
-    def solona_multiple_when(self) ->  DataFrame:
+    def solana_multiple_when(self) ->  DataFrame:
         """Multilpe When Operation with different Conditions"""
         df = self.preprocessing_timeseries()
         df = df.select("volume",
@@ -176,28 +175,28 @@ class SparkJob:
     
     # Like, StartsWith, EndsWith
     
-    def solona_like(self) ->  DataFrame:
+    def solana_like(self) ->  DataFrame:
         """Like Operation on String Colunmns"""
         df = self.preprocessing_timeseries()
         df = df.select("open",when(df.open == 3, "normal").otherwise("not normal").alias("case1"))
         df = df.select("open", "case1", df.case1.like("%not%").alias('likecase1'))
         return df
     
-    def solona_startswith(self) ->  DataFrame:
+    def solana_startswith(self) ->  DataFrame:
         """StartWith Operation on String Colunmns"""
         df = self.preprocessing_timeseries()
         df = df.select("open",when(df.open == 3, "normal").otherwise("not normal").alias("case1"))
         df = df.select("open", "case1", df.case1.startswith("not").alias('startswithcase1'))
         return df
     
-    def solona_endswith(self) ->  DataFrame:
+    def solana_endswith(self) ->  DataFrame:
         """EndsWith Operation on String Colunmns"""
         df = self.preprocessing_timeseries()
         df = df.select("open",when(df.open == 3, "normal").otherwise("not normal").alias("case1"))
         df = df.select("open", "case1", df.case1.endswith("normal").alias('endswithcase1'))
         return df
 
-    def solona_endswith(self) ->  DataFrame:
+    def solana_endswith(self) ->  DataFrame:
         """EndsWith Operation on String Colunmns"""
         df = self.preprocessing_timeseries()
         df = df.select("open",when(df.open == 3, "normal").otherwise("not normal").alias("case1"))
@@ -207,7 +206,7 @@ class SparkJob:
     
     # DROP
     
-    def solona_drop(self) ->  DataFrame:
+    def solana_drop(self) ->  DataFrame:
         """Drop Columns"""
         df = self.preprocessing_timeseries()
         df = df.drop("open","close")
@@ -215,28 +214,28 @@ class SparkJob:
     
     # NULLS
     
-    def solona_replace(self) ->  DataFrame:
+    def solana_replace(self) ->  DataFrame:
         """Replace values"""
         df = self.preprocessing_timeseries()
         df = df.replace(3,3000)
         return df
     
-    def solona_nulls(self):
+    def solana_nulls(self):
         """Replace values with nulls."""
         df = self.preprocessing_timeseries()
         df = df.replace(3,None)
         return df
     
-    def solona_fill_nulls(self) ->  DataFrame:
+    def solana_fill_nulls(self) ->  DataFrame:
         """Replace values with nulls."""
-        df = self.solona_nulls()
+        df = self.solana_nulls()
         df = df.fillna(1000)
         return df
     
 
-    def solona_drop_nulls(self) ->  DataFrame:
+    def solana_drop_nulls(self) ->  DataFrame:
         """Drop Rows with Null based on subset."""
-        df = self.solona_nulls()
+        df = self.solana_nulls()
         print(f'row count: {df.count()}')
         df = df.dropna(subset=["open","close"])
         print(f'row count: {df.count()}')
@@ -246,7 +245,7 @@ class SparkJob:
         
     # FILTER AND UNION
     
-    def solona_split_dataframes(self):
+    def solana_split_dataframes(self):
         """Split Dataframes"""
         df = self.preprocessing_timeseries()
         df = df.withColumn("month", month(df["open_time"]))
@@ -258,9 +257,9 @@ class SparkJob:
         print(f"d3: counts: {df3.count()}")
         return df1, df2, df3
 
-    def solona_union_dataframes(self) -> None:
+    def solana_union_dataframes(self) -> None:
         """Union Dataframes"""
-        df1, df2, df3 = self.solona_split_dataframes()
+        df1, df2, df3 = self.solana_split_dataframes()
         print(f"d1: counts: {df1.count()}")
         print(f"d2: counts: {df2.count()}")
         print(f"d3: counts: {df3.count()}")
@@ -271,30 +270,30 @@ class SparkJob:
         
     # TEMP VIEWS
     
-    def solona_temp_view(self) -> None:
+    def solana_temp_view(self) -> None:
         """Create a Temp View of the dataset"""
         df = self.extract_timeseries()
-        df.createOrReplaceTempView("solona")
-        self.spark.sql("select * from solona").show(3)
+        df.createOrReplaceTempView("solana")
+        self.spark.sql("select * from solana").show(3)
 
     
     # SPARK DATAFRAME TO OTHER DATA STRUCTURES
     
-    def solona_to_pandas(self) -> PandasDataframe:
+    def solana_to_pandas(self) -> PandasDataframe:
         """Spark Dataframe to Pandas Dataframe"""
         df = self.extract_timeseries()
         pdf = df.toPandas()
         return pdf
         
         
-    def solona_to_rdd(self) -> RDD:
+    def solana_to_rdd(self) -> RDD:
         """Spark Dataframe to RDD"""
         df = self.extract_timeseries()
         rdd = df.rdd
         return rdd
     
     
-    def solona_to_json(self) -> str:
+    def solana_to_json(self) -> str:
         """Spark Dataframe to JSON"""
         df = self.extract_timeseries()
         json = df.toJSON().first()
@@ -303,7 +302,7 @@ class SparkJob:
     
     # EXPLAIN
     
-    def solona_explain(self) -> None:
+    def solana_explain(self) -> None:
         """Explain Spark Plan"""
         df = self.preprocessing_timeseries()
         df.explain()
@@ -311,7 +310,7 @@ class SparkJob:
         
     # REPARTITION
     
-    def solona_repartition(self) -> int:
+    def solana_repartition(self) -> int:
         """Repartition Dataframe"""
         df = self.preprocessing_timeseries()
         print(f'partitions = {df.rdd.getNumPartitions()}')
@@ -322,8 +321,61 @@ class SparkJob:
         
     # JOINS
     
-    def solona_join(self) -> DataFrame:
-        """Split Dataframes"""
+    def solana_alias(self, alias: str) -> DataFrame:
+        """Alias dataframe"""
         df = self.preprocessing_timeseries()
-        
+        df = df.alias(alias)
         return df
+    
+    
+    def solana_alias_show(self) -> None:
+        """Select for aliased Dataframe"""
+        df = self.solana_alias(alias='a')
+        df.printSchema()
+        df.select('open_time').show(10)
+        df.select('a.open_time').show(10)
+        
+        
+    def solana_alias_join(self) -> DataFrame:
+        """Alias to dataframes"""
+        
+        # Alias dataframes.
+        dfa = self.solana_alias(alias='a')
+        dfb = self.solana_alias(alias='b')
+        
+        # Print counts.
+        print(f'count: {dfa.count()}')
+        print(f'count: {dfb.count()}')
+        
+        # Test some selects out.
+        dfa = dfa.select('a.open_time','a.open', 'a.close')
+        dfb = dfb.select('b.open_time','b.close')
+
+        # Join dataframes.
+        dfc = dfa.join(dfb, on='open_time', how='left')
+        print(f'count: {dfc.count()}')
+        dfc.show(10)
+
+        # Select on final dataframes.
+        dfc = dfc.select('a.open_time','a.open','b.close')
+        dfc.show(10)
+        return dfc
+    
+    def solana_realias_join(self) -> DataFrame:
+        """
+        Realias dataFrame.
+        
+        >> dfc.select('c.open_time','c.open','c.close').show(10)
+        """
+        
+        print('attempting realias')
+        dfc = self.solana_alias_join()
+        print(f'count: {dfc.count()}')
+        dfc = dfc.alias('c')
+        print(f'count: {dfc.count()}')
+        return dfc
+    
+    
+    
+    
+    
